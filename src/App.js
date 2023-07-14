1303e3e7357c62a52c20496cb742a9e216ed9fbb
cyberloafing <934058879@qq.com>
2023-07-14 19:19:46 +0800

    p8 parse data

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
-import React,{useState, useCallback} from 'react';
+import React,{useState, useCallback, useEffect} from 'react';
 import './App.css';
+import { csv } from 'd3';
+import { message } from './message';
 
 
 const width = 960;
 const height = 500;
-const circleRadius = 50;
+const csvUrl =
+  'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv';
 
-//当代码运行时，首先鼠标的位置是在屏幕的中心
-const initialMousePosition = {x : width/2, y : height/2};
+// const message = data => {
+//     let message = '';
+//     message = message + Math.round(d3.csvFormat(data).length / 1024) + ' kB\n';
+//     message = message + data.length + ' rows\n';
+//     message = message + data.columns.length + ' columns';
+//     return message;
+//   };
+  
 
 
 
 const App = () => {
-  //mousePosition是一个状态，它的初始值是initialMousePosition
-  //setMousePosition是一个函数，它可以改变mousePosition的值，这个函数是由useState返回的，并且它是一个hook
-  //在每次鼠标移动时，handleMouseMove函数会被调用，它会改变mousePosition的值，从而改变circle的位置
-  //但是这种形式不是最佳的，因为这个函数会在每次渲染时都注册一次，这样会导致每次渲染都会有一个新的函数被创建
-  // const [mousePosition, setMousePosition] = useState(initialMousePosition);
-  // const handleMouseMove = event => {
-  //   const { clientX, clientY } = event;
-  //   setMousePosition({x: clientX, y: clientY});
-  // };
-  //但是这种形式不是最佳的，因为这个函数会在每次渲染时都注册一次，这样会导致每次渲染都会有一个新的函数被创建
-  //因此我们需要使用useCallback来避免这种情况
-  //useCallback接受两个参数，第一个参数是一个函数，第二个参数是一个数组，这个数组中的值发生变化时，useCallback才会返回一个新的函数
-  const [mousePosition, setMousePosition] = useState(initialMousePosition);
-  const handleMouseMove = useCallback(event => {
-    const { clientX, clientY } = event;
-    setMousePosition({x: clientX, y: clientY});
-  },[setMousePosition]);
-  return (
-  <svg width={width} height={height} onMouseMove={handleMouseMove} >
-    <circle 
-      cx = {mousePosition.x}
-      cy = {mousePosition.y}
-      r = {circleRadius}
-    />
-  </svg>
-)};
+  const [data, setData] = useState(null);
+  //如果按照下面这个写法，会导致无限循环，因为每次渲染都会重新执行这个函数
+  // csv(csvUrl).then(data => {
+  //   setData(data);
+  // });
+
+  // 因此，我们需要使用useEffect来避免这个问题
+  // useEffect的作用是在渲染完成后执行一些操作
+  useEffect(() => {
+    csv(csvUrl).then(setData);
+  }, []);
+
+  return <pre>{data ? message(data) : 'Loading...'}</pre>;
+
+};
 
 //最后总是导出一个组件
 export default App;
