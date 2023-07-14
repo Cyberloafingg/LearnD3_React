0b64e85e281b1e0981481836ba19d48852a1996f
cyberloafing <934058879@qq.com>
2023-07-14 20:13:54 +0800

    p9 visualizing data to pie

--- src/App.js
+++ src/App.js
@@ -1,40 +1,55 @@
 import React,{useState, useCallback, useEffect} from 'react';
 import './App.css';
-import { csv } from 'd3';
-import { message } from './message';
+import { csv , arc, pie} from 'd3';
 
 
 const width = 960;
 const height = 500;
-const csvUrl =
-  'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv';
+const centerX = width / 2;
+const centerY = height / 2;
+const csvUrl = 'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv';
 
-// const message = data => {
-//     let message = '';
-//     message = message + Math.round(d3.csvFormat(data).length / 1024) + ' kB\n';
-//     message = message + data.length + ' rows\n';
-//     message = message + data.columns.length + ' columns';
-//     return message;
-//   };
-  
-
+const pieArc = arc()
+  .innerRadius(0)
+  .outerRadius(width);
 
 
 const App = () => {
   const [data, setData] = useState(null);
-  //如果按照下面这个写法，会导致无限循环，因为每次渲染都会重新执行这个函数
-  // csv(csvUrl).then(data => {
-  //   setData(data);
-  // });
 
-  // 因此，我们需要使用useEffect来避免这个问题
-  // useEffect的作用是在渲染完成后执行一些操作
   useEffect(() => {
     csv(csvUrl).then(setData);
   }, []);
 
-  return <pre>{data ? message(data) : 'Loading...'}</pre>;
 
+  if(!data){
+    return <pre>Loading...</pre>
+  }
+
+  //为什么pie()的参数是1？
+  // 因为这里的数据是一维的，所以只需要一个参数就可以了
+  const colorPie = pie().value(1);
+  return (
+    <svg width={width} height={height}>
+      <g transform={`translate(${centerX},${centerY})`}>
+        {/* {data.map( (d,i) => (
+          <path 
+            fill={d['RGB hex value']} 
+            d={pieArc({
+              startAngle: i / data.length * 2 * Math.PI,
+              endAngle: 2 * Math.PI
+            })}/>
+        ))} */}
+        {colorPie(data).map( (d) => (
+          <path 
+            fill={d.data['RGB hex value']} 
+            d={pieArc(d)}
+            stroke='blue'
+          />
+        ))}
+      </g>
+    </svg>
+  );
 };
 
 //最后总是导出一个组件
